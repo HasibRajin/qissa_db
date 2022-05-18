14681e9e2dcd29bcfd5973b9064eeb5d3fd8ce81
HasibRajin <thisisrajin@gmail.com>
2022-05-18 01:50:41 -0400

    sdfs

--- build/config/database.js
+++ build/config/database.js
@@ -4,21 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Env_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Env"));
-const url_parse_1 = __importDefault(require("url-parse"));
-const DATABASE_URL = new url_parse_1.default(Env_1.default.get('DATABASE_URL'));
 const databaseConfig = {
     connection: Env_1.default.get('DB_CONNECTION'),
     connections: {
         pg: {
             client: 'pg',
             connection: {
-                host: DATABASE_URL.host,
-                port: Number(''),
-                user: DATABASE_URL.username,
-                password: DATABASE_URL.password,
-                database: DATABASE_URL.pathname.substr(1),
+                host: Env_1.default.get('PG_HOST'),
+                port: Env_1.default.get('PG_PORT'),
+                user: Env_1.default.get('PG_USER'),
+                password: Env_1.default.get('PG_PASSWORD', ''),
+                database: Env_1.default.get('PG_DB_NAME'),
+            },
+            migrations: {
+                naturalSort: true,
             },
             healthCheck: false,
+            debug: false,
         },
     },
 };
--- config/database.ts
+++ config/database.ts
@@ -1,3 +1,56 @@
+// /**
+//  * Config source: https://git.io/JesV9
+//  *
+//  * Feel free to let us know via PR, if you find something broken in this config
+//  * file.
+//  */
+//
+// import Env from '@ioc:Adonis/Core/Env'
+// import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+// import Url from 'url-parse'
+// const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+// console.log('urlCheck:' + DATABASE_URL)
+// const databaseConfig: DatabaseConfig = {
+//   /*
+//   |--------------------------------------------------------------------------
+//   | Connection
+//   |--------------------------------------------------------------------------
+//   |
+//   | The primary connection for making database queries across the application
+//   | You can use any key from the `connections` object defined in this same
+//   | file.
+//   |
+//   */
+//   connection: 'pg',
+//
+//   connections: {
+//     /*
+//     |--------------------------------------------------------------------------
+//     | PostgreSQL config
+//     |--------------------------------------------------------------------------
+//     |
+//     | Configuration for PostgreSQL database. Make sure to install the driver
+//     | from npm when using this connection
+//     |
+//     | npm i pg
+//     |
+//     */
+//     pg: {
+//       client: 'pg',
+//       connection: {
+//         host: DATABASE_URL.host as string,
+//         port: Number(''),
+//         user: DATABASE_URL.username as string,
+//         password: DATABASE_URL.password as string,
+//         database: DATABASE_URL.pathname.substr(1) as string,
+//       },
+//       healthCheck: false,
+//     },
+//   },
+// }
+//
+// export default databaseConfig
+
 /**
  * Config source: https://git.io/JesV9
  *
@@ -7,9 +60,7 @@
 
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
-import Url from 'url-parse'
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
-console.log('urlCheck:' + DATABASE_URL)
+
 const databaseConfig: DatabaseConfig = {
   /*
   |--------------------------------------------------------------------------
@@ -21,7 +72,7 @@ const databaseConfig: DatabaseConfig = {
   | file.
   |
   */
-  connection: 'pg',
+  connection: Env.get('DB_CONNECTION'),
 
   connections: {
     /*
@@ -38,13 +89,17 @@ const databaseConfig: DatabaseConfig = {
     pg: {
       client: 'pg',
       connection: {
-        host: DATABASE_URL.host as string,
-        port: Number(''),
-        user: DATABASE_URL.username as string,
-        password: DATABASE_URL.password as string,
-        database: DATABASE_URL.pathname.substr(1) as string,
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
       },
       healthCheck: false,
+      debug: false,
     },
   },
 }
