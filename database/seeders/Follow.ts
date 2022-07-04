1dca14754f113486130bab0910a16e9b255aa893
HasibRajin <thisisrajin@gmail.com>
2022-07-04 21:40:41 +0600

    ewf

--- database/seeders/Follow.ts
+++ database/seeders/Follow.ts
@@ -4,31 +4,31 @@ import UserRelation from 'App/Models/UserRelation'
 export default class FollowSeeder extends BaseSeeder {
   public async run() {
     await UserRelation.createMany([
-      // {
-      //   user_id: 18,
-      //   relatable_id: 2,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 18,
-      //   relatable_id: 3,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 18,
-      //   relatable_id: 4,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 14,
-      //   relatable_id: 5,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 16,
-      //   relatable_id: 6,
-      //   relatable_type: 'block',
-      // },
+      {
+        user_id: 18,
+        relatable_id: 2,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 18,
+        relatable_id: 3,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 18,
+        relatable_id: 4,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 18,
+        relatable_id: 5,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 18,
+        relatable_id: 6,
+        relatable_type: 'block',
+      },
       {
         user_id: 1,
         relatable_id: 3,
