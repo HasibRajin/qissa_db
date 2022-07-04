e7a0b2a92431b87cfb4b65ae7434eb3d1813d184
HasibRajin <thisisrajin@gmail.com>
2022-07-04 21:34:58 +0600

    ewf

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: ENV_SILENT=true  node ./build/ace migration:run --force
+release: ENV_SILENT=true  node ./build/ace db:seed --files "./database/seeders/UserRelation.ts"
 web: npm run start:prod
--- database/seeders/Follow.ts
+++ database/seeders/Follow.ts
@@ -4,196 +4,241 @@ import UserRelation from 'App/Models/UserRelation'
 export default class FollowSeeder extends BaseSeeder {
   public async run() {
     await UserRelation.createMany([
+      // {
+      //   user_id: 18,
+      //   relatable_id: 2,
+      //   relatable_type: 'follow',
+      // },
+      // {
+      //   user_id: 18,
+      //   relatable_id: 3,
+      //   relatable_type: 'follow',
+      // },
+      // {
+      //   user_id: 18,
+      //   relatable_id: 4,
+      //   relatable_type: 'favourite',
+      // },
+      // {
+      //   user_id: 14,
+      //   relatable_id: 5,
+      //   relatable_type: 'follow',
+      // },
+      // {
+      //   user_id: 16,
+      //   relatable_id: 6,
+      //   relatable_type: 'block',
+      // },
       {
-        user_id: 18,
-        relatable_id: 2,
-        relatable_type: 'follow',
-      },
-      {
-        user_id: 18,
+        user_id: 1,
         relatable_id: 3,
         relatable_type: 'follow',
       },
       {
-        user_id: 18,
+        user_id: 1,
         relatable_id: 4,
         relatable_type: 'favourite',
       },
       {
-        user_id: 14,
+        user_id: 1,
         relatable_id: 5,
         relatable_type: 'follow',
       },
       {
-        user_id: 16,
+        user_id: 1,
         relatable_id: 6,
         relatable_type: 'block',
       },
-      // {
-      //   user_id: 1,
-      //   relatable_id: 3,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 1,
-      //   relatable_id: 4,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 1,
-      //   relatable_id: 5,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 1,
-      //   relatable_id: 6,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 1,
-      //   relatable_id: 7,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 2,
-      //   relatable_id: 1,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 2,
-      //   relatable_id: 4,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 2,
-      //   relatable_id: 5,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 2,
-      //   relatable_id: 6,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 2,
-      //   relatable_id: 7,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 3,
-      //   relatable_id: 2,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 3,
-      //   relatable_id: 1,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 3,
-      //   relatable_id: 4,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 3,
-      //   relatable_id: 5,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 3,
-      //   relatable_id: 6,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 3,
-      //   relatable_id: 7,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 4,
-      //   relatable_id: 2,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 4,
-      //   relatable_id: 3,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 4,
-      //   relatable_id: 1,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 4,
-      //   relatable_id: 6,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 4,
-      //   relatable_id: 7,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 5,
-      //   relatable_id: 2,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 5,
-      //   relatable_id: 3,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 5,
-      //   relatable_id: 4,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 5,
-      //   relatable_id: 1,
-      //   relatable_type: 'favourite',
-      // },
-      // {
-      //   user_id: 5,
-      //   relatable_id: 6,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 5,
-      //   relatable_id: 7,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 6,
-      //   relatable_id: 2,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 6,
-      //   relatable_id: 3,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 6,
-      //   relatable_id: 4,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 6,
-      //   relatable_id: 5,
-      //   relatable_type: 'follow',
-      // },
-      // {
-      //   user_id: 6,
-      //   relatable_id: 1,
-      //   relatable_type: 'block',
-      // },
-      // {
-      //   user_id: 6,
-      //   relatable_id: 7,
-      //   relatable_type: 'follow',
-      // },
+      {
+        user_id: 1,
+        relatable_id: 7,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 2,
+        relatable_id: 1,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 2,
+        relatable_id: 4,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 2,
+        relatable_id: 5,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 2,
+        relatable_id: 6,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 2,
+        relatable_id: 7,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 3,
+        relatable_id: 2,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 3,
+        relatable_id: 1,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 3,
+        relatable_id: 4,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 3,
+        relatable_id: 5,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 3,
+        relatable_id: 6,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 3,
+        relatable_id: 7,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 4,
+        relatable_id: 2,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 4,
+        relatable_id: 3,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 4,
+        relatable_id: 1,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 4,
+        relatable_id: 6,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 4,
+        relatable_id: 7,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 5,
+        relatable_id: 2,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 5,
+        relatable_id: 3,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 5,
+        relatable_id: 4,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 5,
+        relatable_id: 1,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 5,
+        relatable_id: 6,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 5,
+        relatable_id: 7,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 6,
+        relatable_id: 2,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 6,
+        relatable_id: 3,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 6,
+        relatable_id: 4,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 6,
+        relatable_id: 5,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 6,
+        relatable_id: 1,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 6,
+        relatable_id: 7,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 20,
+        relatable_id: 1,
+        relatable_type: 'favourite',
+      },
+      {
+        user_id: 22,
+        relatable_id: 6,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 25,
+        relatable_id: 7,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 8,
+        relatable_id: 2,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 9,
+        relatable_id: 3,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 10,
+        relatable_id: 4,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 20,
+        relatable_id: 5,
+        relatable_type: 'follow',
+      },
+      {
+        user_id: 15,
+        relatable_id: 1,
+        relatable_type: 'block',
+      },
+      {
+        user_id: 12,
+        relatable_id: 7,
+        relatable_type: 'follow',
+      },
     ])
   }
 }
